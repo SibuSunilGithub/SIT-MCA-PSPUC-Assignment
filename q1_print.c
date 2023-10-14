b34b8d79951c77aec85a00662b3d469fb3e1ff2f
Sunil Kumar Sahoo <101491022+SibuSunilGithub@users.noreply.github.com>
2023-10-15 00:31:34 +0530

    Add Answer Code 1
    Write Baa Baa Blacksheep Song

---
+++ q1_print.c
@@ -0,0 +1,11 @@
+// Write a C program to print Bah, Bah Black Sheep song?
+
+#include <stdio.h>
+int main()
+{
+    printf("Bah Bah Blacksheep\n");
+    printf("\tHave You Any Woll\n");
+    printf("Yes Sir Yes Sir,\n");
+    printf("\tThree Bags Full\n");
+    return 0;
+}
