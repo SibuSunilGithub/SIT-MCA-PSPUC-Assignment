a01094c0f86364b7d92508c022c2018e10abb2e0
Sunil Kumar Sahoo <101491022+SibuSunilGithub@users.noreply.github.com>
2023-10-27 23:23:03 +0530

    Add Answer 8
    Given an integer, WAP that display by
    Example:
    
    12345
    2345
    345
    45
    5

---
+++ q08_display_no_structure.c
@@ -0,0 +1,25 @@
+/*
+Given an integer, WAP that display by
+Example:
+
+12345
+2345
+345
+45
+5
+
+*/
+
+#include <stdio.h>
+int main()
+{
+    int num;
+    printf("Please Enter A 5 Digit Integer Number:\n");
+    scanf("%d", &num);
+    printf("%d\n", num);
+    printf("%d\n", num % 10000);
+    printf("%d\n", num % 1000);
+    printf("%d\n", num % 100);
+    printf("%d\n", num % 10);
+    return 0;
+}
