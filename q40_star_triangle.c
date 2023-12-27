a22d7449e11f0058cdc16391b130ccfc448a2ef9
Sunil Kumar Sahoo <101491022+SibuSunilGithub@users.noreply.github.com>
2023-12-27 23:05:27 +0530

    Star Triangle Pattern
             *
          * * *
        * * * * *
      * * * * * * *
    * * * * * * * * *

---
+++ q40_star_triangle.c
@@ -0,0 +1,34 @@
+/*
+Star Pattern
+
+        *
+      * * *
+    * * * * *
+  * * * * * * *
+* * * * * * * * *
+
+*/
+
+#include <stdio.h>
+int main()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 5; j >= i; j--)
+        {
+            printf("  ");
+        }
+
+        for (int j = 0; j <= i; j++)
+        {
+            printf("* ");
+        }
+
+        for (int j = 1; j <= i; j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
