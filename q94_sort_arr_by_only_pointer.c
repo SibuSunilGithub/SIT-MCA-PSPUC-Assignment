b3297a953f85425b686f4a91a28c9bdc2959f2f7
Sunil Kumar Sahoo <101491022+SibuSunilGithub@users.noreply.github.com>
2024-01-03 23:39:29 +0530

    Sort Array By Only Pointer.
    Sorting Of An Array Using Only Pointer.

---
+++ q94_sort_arr_by_only_pointer.c
@@ -0,0 +1,32 @@
+// Sorting Of An Array Using Only Pointer...
+
+#include <stdio.h>
+int main()
+{
+    int n = 5, arr[n], i, j, temp, *p1, *p2;
+    printf("Enter %d Elements In Array: ", n);
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    // Sort array by only pointers
+    for (p1 = arr; p1 < arr + 10 - 1; p1++)
+    {
+        for (p2 = p1 + 1; p2 < arr + 10; p2++)
+        {
+            // swap by temp variable
+            if (*p2 < *p1)
+            {
+                temp = *p1;
+                *p1 = *p2;
+                *p2 = temp;
+            }
+        }
+    }
+    // print the sorted array...
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
